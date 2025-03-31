31e25f71ca0d4a2fd81dc92f8988c1504b10d2bf
Ryan Gladden <gladdenr@byu.edu>
2025-03-31 18:55:46 -0300

    Command switch case

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,17 +1,42 @@
 package server.websocket;
 
+import dataaccess.DataAccessException;
 import org.eclipse.jetty.websocket.api.Session;
 
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static server.websocket.Connection.Role.*;
 
 public class ConnectionManager {
 
-    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, ConcurrentHashMap<String, Connection>> connections = new ConcurrentHashMap<>();
 
-    public void add(String authToken, int gameID, String role, Session session) {
-        Connection.Role teamColor = role == "white" ? WHITE : BLACK;
-        Connection connection = new Connection(authToken, gameID, )
+    public void add(String authToken, int gameID, String role, Session session) throws DataAccessException {
+        Connection.Role teamColor = getRole(role);
+        Connection connection = new Connection(authToken, gameID, teamColor, session);
+        if (connections.containsKey(gameID)) {
+            connections.get(gameID).put(authToken, connection);
+        } else {
+            ConcurrentHashMap<String, Connection> connData = new ConcurrentHashMap<>();
+            connData.put(authToken, connection);
+            connections.put(gameID, connData);
+        }
+    }
+
+//    public void remove(String authToken) {
+//        connections.remove(authToken);
+//    }
+
+    public Connection getConnection(int gameID, String authToken) {
+        return connections.get(gameID).get(authToken);
+    }
+
+    private Connection.Role getRole(String role) {
+        if (Objects.equals(role, "observer")) {
+            return OBSERVER;
+        } else {
+            return Objects.equals(role, "white") ? WHITE : BLACK;
+        }
     }
 }
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,9 +1,30 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
 
 @WebSocket
 public class WebSocketHandler {
 
-    private final ConnectionManage
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onCommand(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case LEAVE -> leaveGame(command);
+            case RESIGN -> resign(command);
+            case CONNECT -> connect(command);
+            case MAKE_MOVE -> makeMove(command, message);
+        }
+    }
+
+    private void leaveGame(UserGameCommand command) {
+
+    }
 }
